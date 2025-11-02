5aace1652aea695b089fe8b159dc96a30bf29f21
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-11-02 20:34:11 +0530

    initial commit

---
+++ lC_sol_and_templates/codes/lec1.cpp
@@ -0,0 +1,19 @@
+// Input marks of five subjects and print the total
+//Input three numbers and print their average.
+//Input an integer and print its next and previous numbers.
+//Input a number and print its square and cube.
+//Input the radius of a circle and print its area and circumference.
+//Take your name as input and print a greeting message.
+
+//utkarsh
+//hello utkarsh
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cin >> name;
+    cout << "Hello ! " << name << endl;
+    return 0;
+}
+//area pi * r * r
+//circumference 2 * 3.14 * r
