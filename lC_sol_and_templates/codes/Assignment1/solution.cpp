9a98d0b1d25fca0e148d8e53e3da3adf465fb389
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-11-04 09:40:21 +0530

    initial commit

---
+++ lC_sol_and_templates/codes/Assignment1/solution.cpp
@@ -0,0 +1,59 @@
+//solution 1
+#include <iostream>
+using namespace std;
+// int main(){
+//     int x, y;
+//     cout << "Enter number x : ";
+//     cin >> x;
+//     cout << "Enter number y : ";
+//     cin >> y;
+//     cout << "Sum of x any y is " << x + y;
+//     return 0;
+// }
+
+//solution 2;
+// int main(){
+//     int x, y, z;
+//     cout << "Enter number x : ";
+//     cin >> x;
+//     cout << "Enter number y : ";
+//     cin >> y;
+//     cout << "Enter number z : ";
+//     cin >> z;
+//     cout << "Average of x, y any z is " << (x + y + z)/3;
+//     return 0;
+// }
+////solution 3;
+// Input temperature in Celsius and convert it to Fahrenheit.
+// int main(){
+//     int temp;
+//     cout << "Enter temperature in celcius : ";
+//     cin >> temp;
+//     cout << "Temperature in fahrenheit is " << (temp * 1.8) + 32 ;
+//     return 0;
+// }
+//solution 4
+//Input radius and height, print the volume of the cylinder.
+// int main(){
+//     int radius, height;
+//     cout << "Enter radius of cylinder : ";
+//     cin >> radius;
+//     cout << "Enter height of cylinder : ";
+//     cin >> height;
+//     cout << "Volume of cylinder is " << 3.14 * radius * radius * height;
+//     return 0;
+// }
+//solution 5 Write a program that prints this exact pattern using cout:
+// int main(){
+//     cout << "Hello, World!" << "\n";
+//     cout << "Welcome to C++ Programming. " << endl;
+//     return 0;
+// }
+//solution 6 Ask the user for their name and print:
+int main(){
+    string name;
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Hi " << name << ", nice to meet you!";
+    return 0;
+}
